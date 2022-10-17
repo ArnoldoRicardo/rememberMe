97545431c505fd288d4d9a134efff38b30ac1e91
ArnoldoRicardo <anyback@gmail.com>
2022-10-17 17:28:28 -0500

    update service

--- backend/src/services/note.test.ts
+++ backend/src/services/note.test.ts
@@ -6,5 +6,8 @@ test('test add note', async () => {
     title: 'soy un titulo'
   }
   const result = await newNota(data.content, data.title)
-  console.log(result)
+  expect(typeof result.id).toBe('number')
+  expect(result.title).toBe(data.title)
+  expect(result.content).toBe(data.content)
+  console.log(result.id)
 })
--- backend/src/services/note.ts
+++ backend/src/services/note.ts
@@ -2,7 +2,6 @@ import { connection } from '../db'
 
 export const newNota = async (content: string, title: string) => {
   const sql = `
-     --sql
         INSERT INTO note (title,content)
         VALUES ('${title}','${content}')
         RETURNING *;
@@ -18,71 +17,71 @@ export const newNota = async (content: string, title: string) => {
   }
 }
 
-// export const updateCreateNote = async (name: string, phone: string) => {
-//   const sql = `
-//      --sql
-//         UPDATE public.person
-//         SET phone='${phone}'
-//         WHERE name in (${name})
-//         RETURNING *;
-//     `
-//   const client = await db.getConnection()
-//   try {
-//     const res = await client.query(sql)
-//     return res[0]
-//   } catch (err: any) {
-//     console.log(err)
-//   } finally {
-//     client.release()
-//   }
-// }
+export const updateCreateNote = async (name: string, phone: string) => {
+  const sql = `
+        UPDATE note
+        SET phone='${phone}'
+        WHERE name in (${name})
+        RETURNING *;
+    `
 
-// export const countNotes = async () => {
-//   const sql = `
-//      --sql
-//      select count(*) from public.person;
-//     `
-//   const client = await db.getConnection()
-//   try {
-//     const res = await client.query(sql)
-//     return res[0].count
-//   } catch (err) {
-//     console.log(err)
-//   } finally {
-//     client.release()
-//   }
-// }
+  await connection.connectAsync()
+  try {
+    const res = await connection.queryAsync(sql)
+    return res[0]
+  } catch (err: any) {
+    console.log(err)
+  } finally {
+    connection.closeAsync()
+  }
+}
 
-// export const getNote = async (title: string) => {
-//   const sql = `
-//      --sql
-//      select * from public.person where "title" = '${title}' limit 1;
-//      `
-//   const client = await db.getConnection()
-//   try {
-//     const res = await client.query(sql)
-//     return res[0]
-//   } catch (err) {
-//     console.log(err)
-//   } finally {
-//     client.release()
-//   }
-// }
+export const countNotes = async () => {
+  const sql = `
+     --sql
+     select count(*) from public.person;
+    `
+  await connection.connectAsync()
+  try {
+    const res = await connection.queryAsync(sql)
+    return res[0].count
+  } catch (err) {
+    console.log(err)
+  } finally {
+    connection.closeAsync()
+  }
+}
 
-// export const getAllNotes = async (hasDelete: boolean) => {
-//   const not = hasDelete ? 'not' : ''
-//   const sql = `
-//      --sql
-//      select * from public.person
-//      where has_delete is ${not} null;
-//     `
-//   const client = await db.getConnection()
-//   try {
-//     const res = await client.query(sql)
-//     return res
-//   } catch (err) {
-//     console.log(err)
-//   } finally {
-//     client.release()
-//   }
-// }
+export const getNote = async (title: string) => {
+  const sql = `
+     --sql
+     select * from public.person where "title" = '${title}' limit 1;
+     `
+  await connection.connectAsync()
+  try {
+    const res = await connection.queryAsync(sql)
+    return res[0]
+  } catch (err) {
+    console.log(err)
+  } finally {
+    connection.closeAsync()
+  }
+}
+
+export const getAllNotes = async (hasDelete: boolean) => {
+  const not = hasDelete ? 'not' : ''
+  const sql = `
+     --sql
+     select * from public.person
+     where has_delete is ${not} null;
+    `
+  await connection.connectAsync()
+  try {
+    const res = await connection.queryAsync(sql)
+    return res
+  } catch (err) {
+    console.log(err)
+  } finally {
+    connection.closeAsync()
+  }
+}
